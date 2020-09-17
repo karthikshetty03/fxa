857ba03250530be1fc6b2c404e8c7586e51a9f7a
Vijay Budhram <vbudhram@gmail.com>
2020-09-17 11:34:42 -0400

    chore(ci): Testing circle ci config

--- packages/fxa-settings/scripts/test-ci.sh
+++ packages/fxa-settings/scripts/test-ci.sh
@@ -1,13 +1,11 @@
 #!/bin/bash -ex
 
-yarn workspaces focus fxa-settings
-PUBLIC_URL=/ INLINE_RUNTIME_CHUNK=false CI=false SKIP_PREFLIGHT_CHECK=true yarn build
-CI=yes SKIP_PREFLIGHT_CHECK=true yarn test
-
 DIR=$(dirname "$0")
 cd "$DIR/.."
 
 mkdir -p config
+
+yarn lint
 
 cd ../../
 mkdir -p ~/.pm2/logs
